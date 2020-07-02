c0976ee302785a5f928e92ef1daac119df00667a
Somprasit <keima2541@gmail.com>
2020-07-02 23:52:18 +0700

    ad fontawsome

--- asset/include/css.php
+++ asset/include/css.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
 <link rel="stylesheet" href="asset\css\style.css">
+<link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MY BomB</title>
 
-    <?php include 'asset\include\css.php'; ?>
+    <?php include 'asset/include/css.php'; ?>
 </head>
 
 <body>
     <div class="container header">
         <div class="row">
             <div class="col-sm">
-                One of three columns
+            <i class="fas fa-home"></i> Home
             </div>
             <div class="col-sm">
                 One of three columns
@@ -23,7 +23,7 @@
             </div>
         </div>
     </div>
-    <?php include 'asset\include\javascript.php' ?>
+    <?php include 'asset/include/javascript.php' ?>
 </body>
 
 </html>
